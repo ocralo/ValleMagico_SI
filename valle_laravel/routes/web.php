8def72d6ba3cfd04a757b4d6c14b60574ca75180
Erik Fernando Loaiza Patiño <39267063+erikloaiza@users.noreply.github.com>
2020-07-21 12:04:40 -0500

    Update web.php

--- valle_laravel/routes/web.php
+++ valle_laravel/routes/web.php
@@ -19,7 +19,7 @@
 Route::get('/', 'Auth\LoginController@showLoginForm')->name('login');
 // Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 // Route::post('login', 'Auth\LoginController@login');
-Route::get('/si/logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
 
 // Registration Routes...
 if ($options['register'] ?? true) {
@@ -39,7 +39,7 @@ if ($options['verify'] ?? false) {
 
 Auth::routes();
 
-Route::get('si/home', 'HomeController@index')->name('home');
+Route::get('home', 'HomeController@index')->name('home');
 
 // Route::get('si/{path?}', 'HomeController@index')->name('home');
 // Route::get('si/{path?}', 'HomeController@index')->name('home');
