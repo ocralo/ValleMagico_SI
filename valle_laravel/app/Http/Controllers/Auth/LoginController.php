0fc6b00aa8026b9ffe984d9c9575e6255e5682c9
Erik Fernando Loaiza Patiño <39267063+erikloaiza@users.noreply.github.com>
2020-07-21 12:10:00 -0500

    Update LoginController.php

--- valle_laravel/app/Http/Controllers/Auth/LoginController.php
+++ valle_laravel/app/Http/Controllers/Auth/LoginController.php
@@ -28,7 +28,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = 'home';
+    protected $redirectTo = '/si/home';
 
     /**
      * Create a new controller instance.
